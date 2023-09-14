6b35d73f49ffbb183e3e2148f7844574bfc2f42d
300079087 <300079087@fvtc.edu>
2023-09-13 17:38:00 -0700

    timerpro
    created timerpro

---
+++ TimerPro/TimerPro/Custom/TimerLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace TimerPro.Custom
+{
+	public class TimerLogic
+	{
+		private int _iSec;
+		private int _iMin;
+		private int _iHour;
+
+		public TimerLogic()
+		{
+			Reset();
+		}
+
+		public void SetTickCounter()
+		{
+			_iSec++;
+			if (_iSec == 60)
+			{
+				_iMin++;
+				_iSec = 0;
+			}
+			if (_iMin == 60)
+			{
+				_iHour++;
+				_iMin = 0;
+			}
+		}
+
+		public void Reset()
+		{
+            _iSec = 0;
+            _iMin = 0;
+            _iHour = 0;
+        }
+
+		public string GetFormatedString()
+		{
+			return _iHour.ToString().PadLeft(2, '0') + ":" + _iMin.ToString().PadLeft(2, '0') + ":" + _iSec.ToString().PadLeft(2, '0'); ;
+		}
+
+
+	}
+}
+
---
+++ TimerPro/TimerPro/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace TimerPro;
+using TimerPro.Custom;
+
+public partial class MainPage : ContentPage
+{
+    TimerLogic timerlogic;
+    bool isRunning;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		Title = "TimerPro";
+        timerlogic = new TimerLogic();
+	}
+
+    [Obsolete]
+    void Start_Clicked(System.Object sender, System.EventArgs e)
+    {
+        btnStart.IsEnabled = false;
+        btnStop.IsEnabled = true;
+        isRunning = true;
+
+        Device.StartTimer(TimeSpan.FromSeconds(1), () => {
+            if (isRunning)
+            {
+                timerlogic.SetTickCounter();
+                lblDisplay.Text = timerlogic.GetFormatedString();
+            }
+
+            return isRunning;
+        });
+    }
+
+    void Stop_Clicked(System.Object sender, System.EventArgs e)
+    {
+        btnStart.IsEnabled = true;
+        btnStop.IsEnabled = false;
+        isRunning = false;
+    }
+
+    void Reset_Clicked(System.Object sender, System.EventArgs e)
+    {
+        isRunning = false;
+        timerlogic.Reset();
+        lblDisplay.Text = timerlogic.GetFormatedString();
+    }
+}
+
+
