6169b5fbe0095ea1fe4061a9e512e65c24b77be3
300079087 <300079087@fvtc.edu>
2023-10-24 07:46:17 -0700

    save stuff
    savestuff completed

---
+++ SaveStuff/SaveStuff/SaveStuff/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SaveStuff
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "Save Stuff";
+        }
+
+        void Save_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Application.Current.Properties["UserName"] = txtUserName.Text;
+        }
+
+        void Read_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (Application.Current.Properties.ContainsKey("UserName"))
+            {
+                txtUserName.Text = Application.Current.Properties["UserName"].ToString();
+            }
+            else
+            {
+                
+            }
+        }
+
+        void Clear_Clicked(System.Object sender, System.EventArgs e)
+        {
+            txtUserName.Text = "";
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Read_Clicked(null, null);
+        }
+    }
+}
